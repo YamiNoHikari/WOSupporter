a38f4faf95b4d12295fe2cbc39822234162d23dd
YamiNoHikari <mario.is.black@gmail.com>
2015-10-27 23:47:40 +0900

    第一次加入GIT本地存儲庫

---
+++ werewolf/Main.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace werewolf
+{
+    partial class WOSupporter
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.YNameLabel = new System.Windows.Forms.Label();
+            this.YNameBox = new System.Windows.Forms.TextBox();
+            this.YCharLabel = new System.Windows.Forms.Label();
+            this.YCharCombo = new System.Windows.Forms.ComboBox();
+            this.HNameLabel = new System.Windows.Forms.Label();
+            this.YCharBox = new System.Windows.Forms.TextBox();
+            this.HNameBox = new System.Windows.Forms.TextBox();
+            this.HCharCombo = new System.Windows.Forms.ComboBox();
+            this.HCharLabel = new System.Windows.Forms.Label();
+            this.ActBox = new System.Windows.Forms.TextBox();
+            this.CopyCharButton = new System.Windows.Forms.Button();
+            this.CreateActButton = new System.Windows.Forms.Button();
+            this.CopyActButton = new System.Windows.Forms.Button();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.HunterBox = new System.Windows.Forms.CheckBox();
+            this.DivinationFailed = new System.Windows.Forms.CheckBox();
+            this.TimeBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // YNameLabel
+            // 
+            this.YNameLabel.AutoSize = true;
+            this.YNameLabel.Location = new System.Drawing.Point(16, 14);
+            this.YNameLabel.Name = "YNameLabel";
+            this.YNameLabel.Size = new System.Drawing.Size(65, 12);
+            this.YNameLabel.TabIndex = 0;
+            this.YNameLabel.Text = "你的名字：";
+            // 
+            // YNameBox
+            // 
+            this.YNameBox.Location = new System.Drawing.Point(84, 10);
+            this.YNameBox.Name = "YNameBox";
+            this.YNameBox.Size = new System.Drawing.Size(128, 21);
+            this.YNameBox.TabIndex = 1;
+            this.YNameBox.TextChanged += new System.EventHandler(this.YNameBox_TextChanged);
+            // 
+            // YCharLabel
+            // 
+            this.YCharLabel.AutoSize = true;
+            this.YCharLabel.Location = new System.Drawing.Point(224, 13);
+            this.YCharLabel.Name = "YCharLabel";
+            this.YCharLabel.Size = new System.Drawing.Size(65, 12);
+            this.YCharLabel.TabIndex = 2;
+            this.YCharLabel.Text = "偽裝角色：";
+            // 
+            // YCharCombo
+            // 
+            this.YCharCombo.FormattingEnabled = true;
+            this.YCharCombo.Items.AddRange(new object[] {
+            "尚未分配",
+            "村人",
+            "人狼",
+            "占卜师",
+            "灵能者",
+            "狂人",
+            "猎人",
+            "共有者",
+            "妖狐",
+            "埋毒者"});
+            this.YCharCombo.Location = new System.Drawing.Point(295, 10);
+            this.YCharCombo.Name = "YCharCombo";
+            this.YCharCombo.Size = new System.Drawing.Size(121, 20);
+            this.YCharCombo.TabIndex = 3;
+            this.YCharCombo.Text = "尚未分配";
+            this.YCharCombo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // HNameLabel
+            // 
+            this.HNameLabel.AutoSize = true;
+            this.HNameLabel.Location = new System.Drawing.Point(16, 70);
+            this.HNameLabel.Name = "HNameLabel";
+            this.HNameLabel.Size = new System.Drawing.Size(65, 12);
+            this.HNameLabel.TabIndex = 4;
+            this.HNameLabel.Text = "苦主名字：";
+            // 
+            // YCharBox
+            // 
+            this.YCharBox.Location = new System.Drawing.Point(18, 37);
+            this.YCharBox.Name = "YCharBox";
+            this.YCharBox.ReadOnly = true;
+            this.YCharBox.Size = new System.Drawing.Size(194, 21);
+            this.YCharBox.TabIndex = 5;
+            this.YCharBox.Text = "你的身份是尚未分配（详细）";
+            this.YCharBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // HNameBox
+            // 
+            this.HNameBox.Enabled = false;
+            this.HNameBox.Location = new System.Drawing.Point(84, 67);
+            this.HNameBox.Name = "HNameBox";
+            this.HNameBox.Size = new System.Drawing.Size(128, 21);
+            this.HNameBox.TabIndex = 6;
+            this.HNameBox.TextChanged += new System.EventHandler(this.HNameBox_TextChanged);
+            // 
+            // HCharCombo
+            // 
+            this.HCharCombo.Enabled = false;
+            this.HCharCombo.FormattingEnabled = true;
+            this.HCharCombo.Items.AddRange(new object[] {
+            "村人",
+            "人狼"});
+            this.HCharCombo.Location = new System.Drawing.Point(295, 67);
+            this.HCharCombo.Name = "HCharCombo";
+            this.HCharCombo.Size = new System.Drawing.Size(121, 20);
+            this.HCharCombo.TabIndex = 7;
+            this.HCharCombo.Text = "人狼";
+            // 
+            // HCharLabel
+            // 
+            this.HCharLabel.AutoSize = true;
+            this.HCharLabel.Location = new System.Drawing.Point(224, 70);
+            this.HCharLabel.Name = "HCharLabel";
+            this.HCharLabel.Size = new System.Drawing.Size(65, 12);
+            this.HCharLabel.TabIndex = 8;
+            this.HCharLabel.Text = "指定角色：";
+            // 
+            // ActBox
+            // 
+            this.ActBox.Location = new System.Drawing.Point(18, 116);
+            this.ActBox.Multiline = true;
+            this.ActBox.Name = "ActBox";
+            this.ActBox.ReadOnly = true;
+            this.ActBox.Size = new System.Drawing.Size(398, 67);
+            this.ActBox.TabIndex = 9;
+            // 
+            // CopyCharButton
+            // 
+            this.CopyCharButton.Location = new System.Drawing.Point(226, 35);
+            this.CopyCharButton.Name = "CopyCharButton";
+            this.CopyCharButton.Size = new System.Drawing.Size(190, 23);
+            this.CopyCharButton.TabIndex = 10;
+            this.CopyCharButton.Text = "複製偽裝角色";
+            this.CopyCharButton.UseVisualStyleBackColor = true;
+            this.CopyCharButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CreateActButton
+            // 
+            this.CreateActButton.Enabled = false;
+            this.CreateActButton.Location = new System.Drawing.Point(18, 189);
+            this.CreateActButton.Name = "CreateActButton";
+            this.CreateActButton.Size = new System.Drawing.Size(93, 23);
+            this.CreateActButton.TabIndex = 11;
+            this.CreateActButton.Text = "生成文本";
+            this.CreateActButton.UseVisualStyleBackColor = true;
+            this.CreateActButton.Click += new System.EventHandler(this.CreateActButton_Click);
+            // 
+            // CopyActButton
+            // 
+            this.CopyActButton.Enabled = false;
+            this.CopyActButton.Location = new System.Drawing.Point(125, 189);
+            this.CopyActButton.Name = "CopyActButton";
+            this.CopyActButton.Size = new System.Drawing.Size(87, 23);
+            this.CopyActButton.TabIndex = 12;
+            this.CopyActButton.Text = "複製文本";
+            this.CopyActButton.UseVisualStyleBackColor = true;
+            this.CopyActButton.Click += new System.EventHandler(this.CopyActButton_Click);
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Location = new System.Drawing.Point(226, 189);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(87, 23);
+            this.AboutButton.TabIndex = 13;
+            this.AboutButton.Text = "關於程式";
+            this.AboutButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(327, 189);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(87, 23);
+            this.ExitButton.TabIndex = 14;
+            this.ExitButton.Text = "退出程式";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // HunterBox
+            // 
+            this.HunterBox.AutoSize = true;
+            this.HunterBox.Enabled = false;
+            this.HunterBox.Location = new System.Drawing.Point(18, 94);
+            this.HunterBox.Name = "HunterBox";
+            this.HunterBox.Size = new System.Drawing.Size(96, 16);
+            this.HunterBox.TabIndex = 15;
+            this.HunterBox.Text = "獵人守護成功";
+            this.HunterBox.UseVisualStyleBackColor = true;
+            // 
+            // DivinationFailed
+            // 
+            this.DivinationFailed.AutoSize = true;
+            this.DivinationFailed.Enabled = false;
+            this.DivinationFailed.Location = new System.Drawing.Point(128, 94);
+            this.DivinationFailed.Name = "DivinationFailed";
+            this.DivinationFailed.Size = new System.Drawing.Size(84, 16);
+            this.DivinationFailed.TabIndex = 16;
+            this.DivinationFailed.Text = "占卜被妨礙";
+            this.DivinationFailed.UseVisualStyleBackColor = true;
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.AutoSize = true;
+            this.TimeBox.Location = new System.Drawing.Point(226, 94);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.Size = new System.Drawing.Size(84, 16);
+            this.TimeBox.TabIndex = 17;
+            this.TimeBox.Text = "增加時間戳";
+            this.TimeBox.UseVisualStyleBackColor = true;
+            // 
+            // WOSupporter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 219);
+            this.Controls.Add(this.TimeBox);
+            this.Controls.Add(this.DivinationFailed);
+            this.Controls.Add(this.HunterBox);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.CopyActButton);
+            this.Controls.Add(this.CreateActButton);
+            this.Controls.Add(this.CopyCharButton);
+            this.Controls.Add(this.ActBox);
+            this.Controls.Add(this.HCharLabel);
+            this.Controls.Add(this.HCharCombo);
+            this.Controls.Add(this.HNameBox);
+            this.Controls.Add(this.YCharBox);
+            this.Controls.Add(this.HNameLabel);
+            this.Controls.Add(this.YCharCombo);
+            this.Controls.Add(this.YCharLabel);
+            this.Controls.Add(this.YNameBox);
+            this.Controls.Add(this.YNameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "WOSupporter";
+            this.Text = "【見習】人狼偽裝師 - 徵求共有者";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label YNameLabel;
+        private System.Windows.Forms.TextBox YNameBox;
+        private System.Windows.Forms.Label YCharLabel;
+        private System.Windows.Forms.ComboBox YCharCombo;
+        private System.Windows.Forms.Label HNameLabel;
+        private System.Windows.Forms.TextBox YCharBox;
+        private System.Windows.Forms.TextBox HNameBox;
+        private System.Windows.Forms.ComboBox HCharCombo;
+        private System.Windows.Forms.Label HCharLabel;
+        private System.Windows.Forms.TextBox ActBox;
+        private System.Windows.Forms.Button CopyCharButton;
+        private System.Windows.Forms.Button CreateActButton;
+        private System.Windows.Forms.Button CopyActButton;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.CheckBox HunterBox;
+        private System.Windows.Forms.CheckBox DivinationFailed;
+        private System.Windows.Forms.CheckBox TimeBox;
+    }
+}
+
---
+++ werewolf/Main.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace werewolf
+{
+    public partial class WOSupporter : Form
+    {
+        string YName = "";//你的名字
+        string YChar = "";//你的角色
+        string HName = "";//苦主的名字
+        string HChar = "";//苦主的角色
+        string TimeString = "";//時間標記
+        int YCharStat = 0;//用於標示各項可用性狀態
+        int YCharID = 0;//用於表示偽裝的第幾種角色
+        public WOSupporter()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            YCharID = YCharCombo.SelectedIndex;//對你的角色賦值
+            YChar = YCharCombo.SelectedItem.ToString();
+            YCharBox.Text = "你的身份是" + YChar + "（详细）";
+            switch (YCharID)
+            {
+                case 0://尚未分配
+                case 1://村人   
+                    YCharStat = 0;
+                    break;
+                case 2://人狼
+                    YCharStat = 1;
+                    break;
+                case 3://占卜师
+                case 4://灵能者
+                    YCharStat = 2;
+                    break;
+                case 5://狂人
+                    YCharStat = 0;
+                    break;
+                case 6://猎人
+                    YCharStat = 1;
+                    break;
+                case 7://共有者
+                case 8://妖狐
+                case 9://埋毒者
+                    YCharStat = 0;
+                    break;
+            }
+            switch (YCharStat)
+            {
+                case 0://類似村民，妖狐等無第二人相關的職業
+                    {
+                        HNameBox.Enabled = false;
+                        HCharCombo.Enabled = false;
+                        CreateActButton.Enabled = false;
+                        CopyActButton.Enabled = false;
+                        TimeBox.Enabled = false;
+                        HunterBox.Enabled = false;
+                        DivinationFailed.Enabled = false;
+                    }
+                    break;
+                case 1://人狼、獵人等存在第二人，但無關第二人職業的職業
+                    {
+                        HNameBox.Enabled = true;
+                        HCharCombo.Enabled = false;
+                        CreateActButton.Enabled = true;
+                        CopyActButton.Enabled = true;
+                        TimeBox.Enabled = true;
+                        if (YCharID == 6) HunterBox.Enabled = true;//判斷是否為獵人
+                        else HunterBox.Enabled = false;
+                        DivinationFailed.Enabled = false;
+            }
+                    break;
+                case 2://占卜師、靈能等和第二人有職業相關的角色
+                    {
+                        HNameBox.Enabled = true;
+                        HCharCombo.Enabled = true;
+                        CreateActButton.Enabled = true;
+                        CopyActButton.Enabled = true;
+                        TimeBox.Enabled = true;
+                        HunterBox.Enabled = false;
+                        if (YCharID == 3) DivinationFailed.Enabled = true;//判斷是否是占卜師
+                        else DivinationFailed.Enabled = false;
+            }
+                    break;
+        }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YNameBox_TextChanged(object sender, EventArgs e)
+        {
+            YName = YNameBox.Text.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(YCharBox.Text);
+        }
+
+        private void CreateActButton_Click(object sender, EventArgs e)
+        {
+            TimeString="\r\n"+ DateTime.Now.ToString("yyyy-MM-dd") + " " + DateTime.Now.ToString("HH:mm:ss");
+            HChar = HCharCombo.SelectedItem.ToString();
+            switch (YCharID)
+            {
+                case 2:
+                    ActBox.Text = "以 " + YName + " 为首的人狼们决定今晚袭击 " + HName +"。";
+                    break;
+                case 3:
+                    {
+                        if (DivinationFailed.Checked == false)
+                            ActBox.Text = YName + " 占卜了 " + HName + " 的身份，他是 " + HChar + "。";
+                        else
+                            ActBox.Text = YName + " 占卜了 " + HName + " 的身份，但是被不知道什么人妨碍了。";
+                    }
+                    break;
+                case 4:
+                    ActBox.Text = "根据 " + YName + " 的灵能结论，被处刑的 " + HName + " 是 " + HChar + "。";
+                    break;
+                case 6:
+                    {
+                        if (HunterBox.Checked == false)
+                            ActBox.Text = YName + " 守护了 " + HName+"。";
+                        else
+                            ActBox.Text = YName + " 成功的守护了 " + HName + "。";
+                    }
+                    break;
+            }
+            if (TimeBox.Checked == true)//檢查是否添加時間戳
+                ActBox.Text = ActBox.Text + TimeString;
+        }
+
+        private void HNameBox_TextChanged(object sender, EventArgs e)
+        {
+            HName = HNameBox.Text.ToString();
+        }
+
+        private void CopyActButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(ActBox.Text);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Werewolf_Close(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+    }
